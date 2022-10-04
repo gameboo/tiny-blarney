f348d47881d18df4b2feb6c879b7d113665fa180
Alexandre Joannou <aj443@cam.ac.uk>
2022-10-04 14:25:53 +0000

    Reshuffled functions in BuildCircuit.hs

--- src/TinyBlarney/Core/BuildCircuit.hs
+++ src/TinyBlarney/Core/BuildCircuit.hs
@@ -28,7 +28,50 @@ err :: String -> a
 err m = error $ "TinyBlarney.Core.BuildCircuit: " ++ m
 
 --------------------------------------------------------------------------------
+
+-- | A built 'Circuit' with its name and netlist
+data Circuit = Circuit { name :: String
+                       , netlist :: Netlist }
+
+-- | Pretty-print a 'Circuit'
+prettyCircuit :: Circuit -> Doc
+prettyCircuit circuit =
+  hang (text "Circuit -" <+> text circuit.name) 2 (pIfc $+$ pNl)
+  where ifc = externalCircuitInterface circuit
+        pIfc = text "- interface:" <+> nest 2 (prettyCircuitInterface ifc)
+        pNl  = text "- netlist:" <+> nest 2 (prettyNetlist circuit.netlist)
+
+-- | 'Show' instance for 'Circuit'
+instance Show Circuit where
+  show = render . prettyCircuit
+
+-- | Return the exposed external 'CircuitInterface' of the given 'Circuit'
+externalCircuitInterface :: Circuit -> CircuitInterface
+externalCircuitInterface Circuit{..} = mconcat (snd <$> ifcs)
+  where
+    ifcs = sortOn fst [ (nId, metaInstanceId nId $ flipCircuitInterface ifc)
+                      | n@MkNet{ instanceId = nId
+                               , primitive = Interface ifc }
+                        <- elems netlist.netlistArray ]
+
+-- | Build a 'Circuit' from a name, a 'CircuitInterface' and a TinyBlarney
+--   circuit function in 'GenCircuit'
+buildCircuitWith :: GenCircuit a => String -> CircuitInterface -> a -> Circuit
+buildCircuitWith nm ifc f = Circuit { name = nm, netlist = nl }
+  where nl = flattenFromRoots $ getCircuitRoots f ifc
+
+-- | Build a 'Circuit' from a name and a TinyBlarney circuit function in
+--   'BuildCircuitIfc' and 'GenCircuit'
+buildCircuit :: (BuildCircuitIfc a, GenCircuit a) => String -> a -> Circuit
+buildCircuit nm f = buildCircuitWith nm (buildCircuitInterface f) f
+
+--------------------------------------------------------------------------------
 -- | Build an interface for a circuit function based on its type signature
+buildCircuitInterface :: BuildCircuitIfc t => t -> CircuitInterface
+buildCircuitInterface = buildCircuitIfc 0
+
+-- backing recursive class implementation
+
 class BuildCircuitIfc t where
   buildCircuitIfc :: Int -> t -> CircuitInterface
 
@@ -40,11 +83,18 @@ instance (Bits a, BuildCircuitIfc t) => BuildCircuitIfc (a -> t) where
     internalInterface @a nm <> buildCircuitIfc (argN + 1) (f undefined)
     where nm = Just $ "arg" ++ show argN
 
-buildCircuitInterface :: BuildCircuitIfc t => t -> CircuitInterface
-buildCircuitInterface = buildCircuitIfc 0
-
 --------------------------------------------------------------------------------
 -- apply circuit function to get root BVs for a blarney circuit
+
+getCircuitRoots :: GenCircuit a => a -> CircuitInterface -> [BV]
+getCircuitRoots f ifc = res.roots
+  where res = genCircuit dfltAcc inPathAndBVs outPaths f
+        inBVs = mkInterfaceBV (flipCircuitInterface ifc) res.outs
+        inPathAndBVs = zip (getPortInPaths ifc) inBVs
+        outPaths = getPortOutPaths ifc
+        dfltAcc = GenCircuitAcc { outs = [], roots = [] }
+
+-- backing recursive class implementation
 
 data GenCircuitAcc = GenCircuitAcc { outs  :: [PathAndBV]
                                    , roots :: [BV] } deriving Show
@@ -70,42 +120,3 @@ instance {-# OVERLAPPABLE #-} (Bits a) => GenCircuit a where
 instance (Bits a, GenCircuit t) => GenCircuit (a -> t) where
   genCircuit acc ((_, bv):rest) outPaths f =
     genCircuit acc rest outPaths (f . unpack $ AsBit bv)
-
-getCircuitRoots :: GenCircuit a => a -> CircuitInterface -> [BV]
-getCircuitRoots f ifc = res.roots
-  where res = genCircuit dfltAcc inPathAndBVs outPaths f
-        inBVs = mkInterfaceBV (flipCircuitInterface ifc) res.outs
-        inPathAndBVs = zip (getPortInPaths ifc) inBVs
-        outPaths = getPortOutPaths ifc
-        dfltAcc = GenCircuitAcc { outs = [], roots = [] }
-
---------------------------------------------------------------------------------
--- produce Circuit Netlist and Interface
-
-data Circuit = Circuit { name :: String
-                       , netlist :: Netlist }
-
-prettyCircuit :: Circuit -> Doc
-prettyCircuit circuit =
-  hang (text "Circuit -" <+> text circuit.name) 2 (pIfc $+$ pNl)
-  where ifc = externalCircuitInterface circuit
-        pIfc = text "- interface:" <+> nest 2 (prettyCircuitInterface ifc)
-        pNl  = text "- netlist:" <+> nest 2 (prettyNetlist circuit.netlist)
-
-instance Show Circuit where
-  show = render . prettyCircuit
-
-externalCircuitInterface :: Circuit -> CircuitInterface
-externalCircuitInterface Circuit{..} = mconcat (snd <$> ifcs)
-  where
-    ifcs = sortOn fst [ (nId, metaInstanceId nId $ flipCircuitInterface ifc)
-                      | n@MkNet{ instanceId = nId
-                               , primitive = Interface ifc }
-                        <- elems netlist.netlistArray ]
-
-buildCircuitWith :: GenCircuit a => String -> CircuitInterface -> a -> Circuit
-buildCircuitWith nm ifc f = Circuit { name = nm, netlist = nl }
-  where nl = flattenFromRoots $ getCircuitRoots f ifc
-
-buildCircuit :: (BuildCircuitIfc a, GenCircuit a) => String -> a -> Circuit
-buildCircuit nm f = buildCircuitWith nm (buildCircuitInterface f) f
