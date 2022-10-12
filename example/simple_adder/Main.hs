99cd1394db42c5c021f99f09ac151efac256a029
Alexandre Joannou <aj443@cam.ac.uk>
2022-10-12 19:49:51 +0000

    Example uses of customInstance in the simple_adder example

--- example/simple_adder/Main.hs
+++ example/simple_adder/Main.hs
@@ -10,28 +10,23 @@ halfAdder x y = (sum, carry)
   where sum = x `bitXor` y
         carry = x `bitAnd` y
 
---instHalfAdder :: Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
---instHalfAdder = customInstanceWithCircuit $ buildCircuit "halfAdder" halfAdder
-
 instHalfAdder :: Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
+--instHalfAdder = halfAdder
+--instHalfAdder = customInstanceWithCircuit $ buildCircuit "halfAdder" halfAdder
 instHalfAdder = customInstanceWith "halfAdder" halfAdder
-
---instHalfAdder :: Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
 --instHalfAdder = customInstance "halfAdder"
 
 fullAdder :: Bit 1 -> Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
 fullAdder cIn x y = (sum, cOut)
+--fullAdder cIn x y = (sum, iterate bitInvert cOut !! 10000)
   where (s0, c0) = instHalfAdder x y
         (sum, c1) = instHalfAdder s0 cIn
         cOut = c0 `bitOr` c1
 
---instFullAdder :: Bit 1 -> Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
---instFullAdder = customInstanceWithCircuit $ buildCircuit "fullAdder" fullAdder
-
 instFullAdder :: Bit 1 -> Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
+--instFullAdder = fullAdder
+--instFullAdder = customInstanceWithCircuit $ buildCircuit "fullAdder" fullAdder
 instFullAdder = customInstanceWith "fullAdder" fullAdder
-
---instFullAdder :: Bit 1 -> Bit 1 -> Bit 1 -> (Bit 1, Bit 1)
 --instFullAdder = customInstance "fullAdder"
 
 rawCarryChainAdder :: Bit 1 -> Bit n -> Bit n -> (Bit n, Bit 1)
@@ -53,5 +48,5 @@ main = do
   putStrLn $ show c
   putStrLn "--------------------------------------------------"
   putStrLn v
-  where c = buildCircuit "carryChainAdder" $ carryChainAdder @8
+  where c = buildCircuit "carryChainAdder" $ carryChainAdder @128
         v = writeVerilogModule c
