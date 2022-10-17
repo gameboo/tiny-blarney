96126e2f19b12bb4a7996cd513871984bac24cdf
Alexandre Joannou <aj443@cam.ac.uk>
2022-10-17 11:57:46 +0000

    Update in-haskell simulation backend place-holder

--- src/TinyBlarney/Backends/Simulation/Haskell.hs
+++ src/TinyBlarney/Backends/Simulation/Haskell.hs
@@ -21,5 +21,8 @@ err m = error $ "TinyBlarney.Backends.Simulation.Haskell: " ++ m
 
 -- | Build a 'Simulator' using in haskell interpretation of the 'Circuit''s
 --   netlist
-buildSimulatorWithHaskell :: Map String Simulator -> Circuit -> Simulator
-buildSimulatorWithHaskell = err $ "Not implemented yet"
+buildSimulatorWithHaskell :: Map String Simulator -> Circuit -> IO Simulator
+buildSimulatorWithHaskell sims c@Circuit{backingImplementation = Netlist nl} =
+  err $ "Haskell Simulator not supported for: " ++ show c
+buildSimulatorWithHaskell _ c =
+  err $ "Haskell Simulator not supported for: " ++ show c
