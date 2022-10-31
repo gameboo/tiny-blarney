0d547e4501f0c21d0b78521a085ff48f75cda11a
Alexandre Joannou <aj443@cam.ac.uk>
2022-10-31 16:52:52 +0000

    export simulation Types from Backends.hs

--- src/TinyBlarney/Backends.hs
+++ src/TinyBlarney/Backends.hs
@@ -13,11 +13,13 @@ This is the set of TinyBlarney backends.
 module TinyBlarney.Backends (
   module TinyBlarney.Backends.CodeGeneration.SMT
 , module TinyBlarney.Backends.CodeGeneration.Verilog
+, module TinyBlarney.Backends.Simulation.Types
 , module TinyBlarney.Backends.Simulation.SimulatorBuild
 , module TinyBlarney.Backends.Verification.SMT
 ) where
 
 import TinyBlarney.Backends.CodeGeneration.SMT
 import TinyBlarney.Backends.CodeGeneration.Verilog
+import TinyBlarney.Backends.Simulation.Types
 import TinyBlarney.Backends.Simulation.SimulatorBuild
 import TinyBlarney.Backends.Verification.SMT
